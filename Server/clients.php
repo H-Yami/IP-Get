5ddac4285524e2cacf32c7e2d184aaac0af017f0
H-Yami <122750794+H-Yami@users.noreply.github.com>
2023-01-24 17:41:30 +0300

    Readable dashboard
    The dashboard now is much more readable

--- Server/clients.php
+++ Server/clients.php
@@ -9,22 +9,44 @@ if (isset($_SESSION['id'])) {// checking if we have a session
     if($con === false){
         die("ERROR: Could not connect to the database. " . mysqli_connect_error());
     }
+    //beaware what follows is a not so pretty echoing of html
+    //if you have a better idea PLEASE tell me
+    echo("<html>
+    <head>
+    <style>
+    body {
+        background-color: #131516;
+        position: relative;
+      }
+    td {
+  color: lightgrey; border: 2px solid grey; border-radius: 10px; padding: 6px;
+    }
+    th{
+  color: white; border: 2px solid white; border-radius: 10px; padding: 6px;
+    }
+    </style>
+    </head>
+    <body>
+    <table style='margin-left: auto; margin-right: auto;'>
+  <tr>
+  <center><th>User</th></center>
+  <center><th>IP</th></center>
+  </tr>
+  ");
     if($clients = $con->prepare('select ip, user , max( time ) from '.$ipTable.' group by ip , user ')){//getting the latest ip for each user
         $clients->execute(); //executing the prepared sql statement
         while($row=$clients->fetch(PDO::FETCH_ASSOC)){//going through each resulting row
-        echo($row['user'].' | '.$row['ip']);
+        echo("<tr>");
+        echo("<td><center>".htmlspecialchars($row['user'])."</center></td>");
+        echo("<td><center>".htmlspecialchars($row['ip'])."</center></td>");
+        echo("</tr>");
         }
     }
-
+    echo("</table> </body></html>");
 }else{//if the user isn't logged in, redirect to login
     header('Location: index.html');
 	die();
 }
-
-
-
-
-
 
 }else{//if there is no session, redirect to login
     header('Location: index.html');
